bbdef7748f473e82be84c36330572d92aa3bc1a8
masterdev <{ID}+{username}@users.noreply.github.com>
2024-04-21 13:44:06 +0000

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { principalRoute } from './enviroments/route';
 import { LoginComponent } from './features/login/login.component';
 
 export const routes: Routes = [
-  { path: '',
-    loadComponent: () => import('./core/components/layout/layout.component'),
-    children: [
-      { path: '', loadComponent: () => import('./features/home-page/home-page.component') },
-    ]
-  },
-  { path: principalRoute.login, component: LoginComponent },
+  // { path: '',
+  //   loadComponent: () => import('./core/components/layout/layout.component'),
+  //   children: [
+  //     { path: '', loadComponent: () => import('./features/home-page/home-page.component') },
+  //   ]
+  // },
+  { path: '', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
